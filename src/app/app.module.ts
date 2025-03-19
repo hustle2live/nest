948167167d8da77663e7010ab748fe198031dce6
hustle2live <kuz.v@ukr.net>
2025-03-19 14:08:33 +0200

    added providers: TodoService, UserService in Appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { PrismaModule } from 'src/prisma-db/prisma.module';
 import { TodosController } from 'src/todos/todos.controller';
 import { UsersController } from 'src/users/users.controller';
+import { TodoService } from 'src/todos/todos.service';
+import { UserService } from 'src/users/users.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AppController, TodosController, UsersController],
-  providers: [AppService],
+  providers: [AppService, TodoService, UserService],
 })
+
 export class AppModule {}
